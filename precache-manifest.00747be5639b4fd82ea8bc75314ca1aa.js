4c210c6622364ac06e2c66f92a51619c5b1c7359
Grace Akinola <adefunkeaa@gmail.com>
2020-05-14 22:50:45 -0400

    Updates

---
+++ precache-manifest.00747be5639b4fd82ea8bc75314ca1aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d024baf0dec32a6bf1fe838d7d2bac37",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "0a4136e0a14b7d84fca3",
+    "url": "/web/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "611b0f657292bebf93ba",
+    "url": "/web/static/js/2.da4cdb21.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/web/static/js/2.da4cdb21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4136e0a14b7d84fca3",
+    "url": "/web/static/js/main.7ad1444b.chunk.js"
+  },
+  {
+    "revision": "554c985fc66173ffde23",
+    "url": "/web/static/js/runtime-main.d129af97.js"
+  },
+  {
+    "revision": "dbf3bbdf38fbd7d78d76f78e0ae822d2",
+    "url": "/web/static/media/ima_3.dbf3bbdf.jpg"
+  },
+  {
+    "revision": "77b66469c28d87f075d38e8078a73f62",
+    "url": "/web/static/media/ima_5.77b66469.jpg"
+  },
+  {
+    "revision": "815fcd504a5c3fc492ddc04036dd6ea7",
+    "url": "/web/static/media/ima_6.815fcd50.jpg"
+  },
+  {
+    "revision": "a2dbb98654d49548fb1e0f9f516bc992",
+    "url": "/web/static/media/ima_9.a2dbb986.jpg"
+  }
+]);
