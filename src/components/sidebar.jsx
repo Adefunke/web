4a7bd0dce89f5a2a7d83f16843488fdf79d8b747
Adefunke <adefunkeaa@gmail.com>
2021-08-07 14:34:56 -0400

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -7,7 +7,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(' + require('./images/ima_9.jpg') + ')'}} />
+              <div className="author-img" style={{backgroundImage: 'url(' + require('./images/face.jpg') + ')'}} />
               <h1 id="colorlib-logo"><a href="index.html">Adefunke Akinola</a></h1>
               <span className="email"><i className="icon-mail"></i> adefunkeaa@gmail.com</span>
             </div>
